9b2b5fe88cf14659b19ce0838de75f68b3d6653b
1031531798 <1031531798@qq.com>
2022-12-05 23:54:39 +0800

    feat(加入路径别名): 登录页样式开发

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,16 @@
 import { FC } from 'react'
 import { usePrefixCls } from 'src/hook/useConfig';
 import './login.scss'
+import BgImage from '@/assets/image/login/login-bg.svg'
 const Index: FC = () => {
+
     const prefixCls = usePrefixCls('login')
     return (
         <div className={prefixCls}>
             <div className={`${prefixCls}-cover`}>
-                <img src={'@/assets/image/login/login-bg.sv'} alt={'cover'} />
+                <div className={`${prefixCls}-cover-point`}></div>
+                <div className={`${prefixCls}-cover-point`}></div>
+                <div className={`${prefixCls}-cover-point`}></div>
             </div>
         </div>
     )
