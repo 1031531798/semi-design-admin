3dd013837b5bdb3d6412fd63a59cdd7735416c78
1031531798 <1031531798@qq.com>
2022-12-16 09:41:57 +0800

    refactor(组件封装): 封装暗黑模式组件和国际化组件
    优化了 暗黑模式组件 去除了多余的变量

---
+++ src/pages/layout/components/header/colorMode.tsx
@@ -0,0 +1,42 @@
+import {Button, Tooltip} from "@douyinfe/semi-ui";
+import {useEffect, useMemo, useState} from "react";
+import useStore from "../../../../store";
+import {ColorModeType} from "../../../../config/type";
+import {IconMoon, IconSun} from "@douyinfe/semi-icons";
+import {useLocale} from "../../../../locales";
+
+const ColorMode = () => {
+    const setMode = useStore(state => state.setColorMode)
+    const colorMode = useStore(state => state.colorMode)
+    const { formatMessage } = useLocale()
+    // 变更 semi ui 暗黑模式
+    function setSemiColorMode () {
+        const body = document.body;
+        if (colorMode === ColorModeType.dark) {
+            body.setAttribute('theme-mode', 'dark');
+        }else {
+            body.removeAttribute('theme-mode');
+        }
+    }
+    // 设置mode icon
+    const getModeIcon = useMemo(() => {
+        setSemiColorMode()
+        return colorMode === ColorModeType.light ? <IconMoon /> : <IconSun />
+    }, [colorMode])
+    // 设置对应mode 提示
+    const getModeTooltip = useMemo(() => {
+        return colorMode === ColorModeType.light ? formatMessage({ id: 'web.setting.color-dark' }) : formatMessage({ id: 'web.setting.color-light' })
+    }, [colorMode, formatMessage])
+    function changeColorMode() {
+        const mode = colorMode === ColorModeType.light ? ColorModeType.dark : ColorModeType.light
+        setMode(mode)
+    }
+
+    return (
+        <Tooltip content={getModeTooltip}>
+            <Button icon={getModeIcon} onClick={changeColorMode} style={{ width: 40, marginRight: 20 }} />
+        </Tooltip>
+    )
+}
+
+export default  ColorMode
--- src/pages/layout/components/header/index.tsx
+++ src/pages/layout/components/header/index.tsx
@@ -8,45 +8,12 @@ import useStore from 'src/store';
 import { ColorModeType } from '../../../../config/type';
 import { isString } from '../../../../utils/is';
 import { useLocale } from '../../../../locales';
+import ColorMode from "./colorMode";
+import LocaleMode from "./localeMode";
 
 const { Header } = Layout
 const Index = () => {
   const prefixCls = usePrefixCls('layout-header')
-  const setLocaleMode = useStore(state => state.setLocaleMode)
-  const setMode = useStore(state => state.setColorMode)
-  function changeLocale(value: string | number | any[] | Record<string, any> | undefined) {
-    if (isString(value)) {
-      setLocaleMode(value)
-    }
-  }
-  const { formatMessage } = useLocale()
-  const [colorMode, setColorMode] = useState(useStore(state => state.colorMode))
-  useEffect(() => {
-    setSemiColorMode()
-  })
-  const getModeIcon = useMemo(() => {
-    return colorMode === ColorModeType.light ? <IconMoon /> : <IconSun />
-  }, [colorMode])
-
-  const getModeTooltip = useMemo(() => {
-    return colorMode === ColorModeType.light ? formatMessage({ id: 'web.setting.color-dark' }) : formatMessage({ id: 'web.setting.color-light' })
-  }, [colorMode, formatMessage])
-
-  function changeColorMode() {
-    const mode = colorMode === ColorModeType.light ? ColorModeType.dark : ColorModeType.light
-    setColorMode(mode)
-    setMode(mode)
-  }
-
-  function setSemiColorMode () {
-    const body = document.body;
-    if (colorMode === ColorModeType.dark) {
-      body.setAttribute('theme-mode', 'dark');
-    }else {
-      body.removeAttribute('theme-mode');
-    }
-  }
-
 
   return (
     <Header className={prefixCls}>
@@ -58,13 +25,8 @@ const Index = () => {
         header={<HeaderNav></HeaderNav>}
         footer={
           <>
-            <Tooltip content={getModeTooltip}>
-              <Button icon={getModeIcon} onClick={changeColorMode} style={{ width: 40, marginRight: 20 }} />
-            </Tooltip>
-            <Select defaultValue='zh_CN' onChange={changeLocale} style={{ width: 120, marginRight: 10 }} insetLabel={<IconLanguage />}>
-              <Select.Option value='zh_CN'>中文</Select.Option>
-              <Select.Option value='en_GB'>English</Select.Option>
-            </Select>
+            <ColorMode />
+            <LocaleMode />
             <Dropdown
               position="bottom"
               render={
