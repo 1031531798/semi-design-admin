19887b98a68c678c61b41f6c692ecd03d9adc89c
1031531798 <1031531798@qq.com>
2023-03-18 00:50:53 +0800

    style(tailwindcss Preflight 本地添加): Preflight 样式本地添加 可自定义化

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import {IntlProvider } from 'react-intl'
 import {localeConfig} from './locales'
 import { BrowserRouter} from 'react-router-dom'
 import {RenderRouter} from './router'
+import './styles/tailwind.css'
 import './styles/common.scss'
 import useStore from 'src/store';
 import zh_CN from '@douyinfe/semi-ui/lib/es/locale/source/zh_CN';
 import en_GB from '@douyinfe/semi-ui/lib/es/locale/source/en_GB';
 import 'animate.css';
-import './styles/tailwind.css'
 
 function App() {
 const locale: string = useStore(state => state.localeMode)
