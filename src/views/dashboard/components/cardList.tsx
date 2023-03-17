9b8aec4c113c9aadb260d5a1ae3eb6e6f5cf5f79
1031531798 <1031531798@qq.com>
2023-03-17 16:51:31 +0800

    perf(仪表盘开发): cardList组件 更换图标

--- src/views/dashboard/components/cardList.tsx
+++ src/views/dashboard/components/cardList.tsx
@@ -2,13 +2,15 @@
 import {Col, Row} from "@douyinfe/semi-ui";
 import NumberCard from "../../../components/card/numberCard";
 import {IoWallet} from "react-icons/io5";
+import {GrMoney} from "react-icons/gr";
+import {GiReceiveMoney, GiPayMoney, GiTakeMyMoney} from "react-icons/gi";
 
 export const HomeCardList = () => {
     const cards = [
         {icon: <IoWallet size={30} color={'#1fcb4f'} />, title: '余额', value: '41210'},
-        {icon: <IoWallet size={30} color={'#1fcb4f'} />, title: '收益', value: '3522'},
-        {icon: <IoWallet size={30} color={'#1fcb4f'} />, title: '费用', value: '5819'},
-        {icon: <IoWallet size={30} color={'#1fcb4f'} />, title: '存款', value: '30020'},
+        {icon: <GiReceiveMoney size={30} color={'#1fcb4f'} />, title: '收益', value: '3522'},
+        {icon: <GiPayMoney size={30} color={'#1fcb4f'} />, title: '费用', value: '5819'},
+        {icon: <GiTakeMyMoney size={30} color={'#1fcb4f'} />, title: '存款', value: '30020'},
     ]
     return (
         <>
