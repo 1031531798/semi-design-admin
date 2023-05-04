edc42c89f9c18a3c12a6dad20373dc1cdeb6f69e
林滋剑 <1031531798@qq.com>
2023-05-04 10:33:17 +0800

    build(更新react 版本为18 ): 升级兼容ts 5.0 防止eslint报错;eslint 配置优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,29 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        // 使用 2 个空格缩进
-        "indent": ["error", 2],
-    }
-}
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "overrides": [
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "@typescript-eslint"
+  ],
+  "rules": {
+    // 使用 2 个空格缩进
+    "indent": ["error", 2],
+    // 取消禁止使用require引入提示
+    '@typescript-eslint/no-var-requires': 0
+  },
+};
