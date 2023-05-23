ff71586fb2a1823a8520624aba1b08c603903267
林滋剑 <1031531798@qq.com>
2023-05-24 00:46:21 +0800

    refactor(路由): 路由前置守卫优化判断

--- src/router/route/guardRoute.tsx
+++ src/router/route/guardRoute.tsx
@@ -1,25 +1,15 @@
 import { useLocation, Navigate } from "react-router-dom";
 import useStore from "../../store/index";
-import { webSettings } from "../../config/setting";
+import { webSettings } from "@/config/setting";
 import { DisposeRouteProps } from "./disposeRoute";
 import { Toast } from "@douyinfe/semi-ui";
+import { useEffect } from "react";
 
 // 路由前置守卫
 const GuardRoute = ({ element }: DisposeRouteProps) => {
   const { token } = useStore();
-  const { pathname } = useLocation();
   // token鉴权
-  if (!token) {
-    // 如果token不存在就返回login
-    Toast.warning("token 不能为空");
-    return <Navigate to={{ pathname: "/login" }} replace />;
-  }
-  // 默认首页
-  return pathname === "/" ? (
-    <Navigate to={{ pathname: webSettings.defaultRouter }} replace />
-  ) : (
-    element
-  );
+  return token ? element : <Navigate to={{ pathname: "/login" }} replace />;
 };
 
 export default GuardRoute;
