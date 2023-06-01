48a64b6c0177d71122fbb94aba7f6f0c1bd67d46
林滋剑 <1031531798@qq.com>
2023-06-01 17:48:25 +0800

    refactor(代码清理): 删除无用引用

--- src/router/route/guardRoute.tsx
+++ src/router/route/guardRoute.tsx
@@ -1,9 +1,7 @@
-import { useLocation, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import useStore from "../../store/index";
-import { webSettings } from "@/config/setting";
 import { DisposeRouteProps } from "./disposeRoute";
-import { Toast } from "@douyinfe/semi-ui";
-import { useEffect } from "react";
+
 
 // 路由前置守卫
 const GuardRoute = ({ element }: DisposeRouteProps) => {
